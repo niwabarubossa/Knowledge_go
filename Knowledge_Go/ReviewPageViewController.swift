e6869740b025d7fefa2c59d91cb4226d6cc32a6a
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-06 21:16:13 +0900

    create reviewpage vc

---
+++ Knowledge_Go/ReviewPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewPageViewController.swift
+//  Knowledge_Go
+//
+//  Created by Ni Ryogo on 2021/03/06.
+//
+
+import UIKit
+
+class ReviewPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func reviewButtonTapped(_ sender: Any) {
+        print("reviewButtonTapped")
+    }
+}
