11c85e2825fec59c0674c07619ae3f4e53283807
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-06 15:36:13 +0900

    Initial Commit

---
+++ Knowledge_Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Knowledge_Go
+//
+//  Created by Ni Ryogo on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
