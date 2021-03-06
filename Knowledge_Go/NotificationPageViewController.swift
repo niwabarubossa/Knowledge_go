9d72fdf47c366d6deb288a9f411de2d848389c52
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-06 21:23:56 +0900

    create notification page vc

---
+++ Knowledge_Go/NotificationPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationPageViewController.swift
+//  Knowledge_Go
+//
+//  Created by Ni Ryogo on 2021/03/06.
+//
+
+import UIKit
+
+class NotificationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func notificationPageButtonTapped(_ sender: Any) {
+        print("notificationPageButtonTapped")
+    }
+}
