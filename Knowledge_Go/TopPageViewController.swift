3d72a1839651707c4acc64d2f8b14434a2aa0214
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-06 21:07:56 +0900

    create toppage vc

---
+++ Knowledge_Go/TopPageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TopPageViewController.swift
+//  Knowledge_Go
+//
+//  Created by Ni Ryogo on 2021/03/06.
+//
+
+import UIKit
+
+class TopPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("button tapped!")
+    }
+    
+}
