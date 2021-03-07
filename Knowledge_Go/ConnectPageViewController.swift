dd916ca920857a5f04e5d46a18eaa6f4d691b170
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-07 11:34:04 +0900

    label hidden ok

--- Knowledge_Go/ConnectPageViewController.swift
+++ Knowledge_Go/ConnectPageViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ConnectPageViewController: UIViewController {
 
+    
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var questionLabel1: UILabel!
+    @IBOutlet weak var questionLabel2: UILabel!
+    @IBOutlet weak var questionLabel3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,5 +23,30 @@ class ConnectPageViewController: UIViewController {
     
     @IBAction func backButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func tappedQuestionSwitch(_ sender: UISwitch) {
+        let label_array = [questionLabel1,questionLabel2,questionLabel3]
+        if sender.isOn {
+            for item in label_array {
+                item?.isHidden = false
+            }
+        }else{
+            for item in label_array {
+                item?.isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func tappedReviewButton(_ sender: UISwitch) {
+        let label_array = [reviewLabel]
+        if sender.isOn {
+            for item in label_array {
+                item?.isHidden = false
+            }
+        }else{
+            for item in label_array {
+                item?.isHidden = true
+            }
+        }
     }
 }
