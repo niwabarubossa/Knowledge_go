68201cda1b07cdc81987b4f3a4fd37e08dbca2f1
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-07 10:31:01 +0900

    コメントアウトの追加

--- Knowledge_Go/ViewController.swift
+++ Knowledge_Go/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
                     self.moveMapKit()
 //                    座標の表示
 //                    locManager.startUpdatingLocation()
+//                    コメントアウトの追加
                     break
                 default:
                     break
