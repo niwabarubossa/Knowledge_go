560a59b979833ef955c8bd0e66dfbb9d401baafe
niwabarubossa <niwabarubossa@yahoo.co.jp>
2021-03-07 12:17:20 +0900

    tableview setup cell connect error

---
+++ Knowledge_Go/NotifiactionQuestionCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotifiactionQuestionCell.swift
+//  Knowledge_Go
+//
+//  Created by Ni Ryogo on 2021/03/07.
+//
+
+import UIKit
+
+class NotifiactionQuestionCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Knowledge_Go/NotificationPageViewController.swift
+++ Knowledge_Go/NotificationPageViewController.swift
@@ -9,17 +9,45 @@ import UIKit
 
 class NotificationPageViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    var dataSource:[Dictionary<String,String>] = [
+        ["test":"test"],
+        ["test":"test"],
+        ["test":"test"],
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func notificationPageButtonTapped(_ sender: Any) {
-        print("notificationPageButtonTapped")
+        self.tableViewSetup()
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
 }
+
+extension NotificationPageViewController: UITableViewDataSource,UITableViewDelegate {
+    
+    private func tableViewSetup(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UINib(nibName: "NotifiactionQuestionCell", bundle: nil), forCellReuseIdentifier: "NotifiactionQuestionCell")
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NotifiactionQuestionCell", for: indexPath ) as! NotifiactionQuestionCell
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+}
+
+
+
